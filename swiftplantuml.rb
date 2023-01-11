b46a5ae3e878ca0c7fed5aade64b35559d350854
Vilian Iaumbayev <vilian.iaumbayev@indriver.com>
2023-01-12 00:18:49 +0530

    add version fixes

--- swiftplantuml.rb
+++ swiftplantuml.rb
@@ -1,7 +1,7 @@
 class Swiftplantuml < Formula
   desc "Generate UML class diagrams from Swift sources"
-  homepage "https://github.com/MarcoEidinger/SwiftPlantUML"
-  url "https://github.com/MarcoEidinger/SwiftPlantUML/archive/0.2.0.tar.gz"
+  homepage "https://github.com/WillianLike/SwiftPlantUML"
+  url "https://github.com/WillianLike/SwiftPlantUML/archive/0.2.0.tar.gz"
   sha256 "81c853463325b20197dbb28ebbd36d9a21f2c2a1843e347dbbe3d5a200bfa350"
   license "MIT"
 
